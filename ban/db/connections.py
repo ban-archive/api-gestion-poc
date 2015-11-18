09f2cc2ee13e1c01f2bb50c4d68b7e6addebe701
Yohan Boniface <yb@enix.org>
2015-11-18 13:49:53 +0100

    Add env var for db port

--- ban/db/connections.py
+++ ban/db/connections.py
@@ -8,6 +8,7 @@ default = PostgresqlExtDatabase(
     user=os.environ.get('DB_USER'),
     password=os.environ.get('DB_PASSWORD'),
     host=os.environ.get('DB_HOST'),
+    port=os.environ.get('DB_PORT'),
 )
 test = PostgresqlExtDatabase(
     'test_' + os.environ.get('DB_NAME', 'ban'),
@@ -15,4 +16,5 @@ test = PostgresqlExtDatabase(
     user=os.environ.get('DB_USER'),
     password=os.environ.get('DB_PASSWORD'),
     host=os.environ.get('DB_HOST'),
+    port=os.environ.get('DB_PORT'),
 )
