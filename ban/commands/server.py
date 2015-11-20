877917f10b5884d199cc2cccfb47eddf6dbe4ca2
Yohan Boniface <yb@enix.org>
2015-11-20 10:44:34 +0100

    Commands need to accept kwargs now

--- ban/commands/server.py
+++ ban/commands/server.py
@@ -5,7 +5,7 @@ from ban.http import application
 
 
 @command
-def run(port=5959, host='0.0.0.0'):
+def run(port=5959, host='0.0.0.0', **kwargs):
     """Run BAN server (for demo and dev only)."""
     httpd = make_server(host, port, application)
     print("Serving HTTP on {}:{}...".format(host, port))
