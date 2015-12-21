e2d9caaf1198d2b7919c0f1cc221178a5242eb73
Yohan Boniface <yb@enix.org>
2015-12-21 11:29:57 +0100

    Remove useless middleware

--- ban/http/middlewares.py
+++ ban/http/middlewares.py
@@ -9,16 +9,6 @@ class CorsMiddleware:
         resp.set_header('Access-Control-Allow-Headers', 'X-Requested-With')
 
 
-class RouteMiddleware:
-    # Use me when https://github.com/falconry/falcon/pull/651 get merged.
-
-    def process_resource(self, req, resp, resource, params):
-        if 'id' in params:
-            *identifier, id = params['id'].split(':')
-            params['identifier'] = identifier[0] if identifier else 'id'
-            params['id'] = id
-
-
 class SessionMiddleware:
 
     def process_response(self, req, resp, resource):
