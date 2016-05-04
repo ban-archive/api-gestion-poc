eb081372982308339052af2138ac466bfb548f99
Yohan Boniface <yb@enix.org>
2016-05-04 14:32:55 +0200

    Remove unused code in test_commands

--- ban/tests/commands/test_commands.py
+++ ban/tests/commands/test_commands.py
@@ -12,9 +12,6 @@ from ban.tests import factories
 
 
 def test_import_municipalities(staff, config):
-    # When loaded from tests, ProcessPoolExecutor fail when more processes are
-    # used than the chunk passed to map.
-    config.get('WORKERS', 1)
     path = Path(__file__).parent / 'data/municipalities.csv'
     municipalities(path)
     assert len(models.Municipality.select()) == 4
@@ -22,7 +19,6 @@ def test_import_municipalities(staff, config):
 
 
 def test_import_municipalities_can_be_filtered_by_departement(staff, config):
-    config.get('WORKERS', 1)
     path = Path(__file__).parent / 'data/municipalities.csv'
     municipalities(path, departement=33)
     assert len(models.Municipality.select()) == 1
