55335cbae3edc07adc64d65416f6b87bf37f42c8
philippe VESSIERE <fly_fy@popstar.com>
2016-06-23 11:08:30 +0200

    Delete BaseGroup class

--- ban/tests/http/test_housenumber.py
+++ ban/tests/http/test_housenumber.py
@@ -35,11 +35,13 @@ def test_get_housenumber_with_unknown_id_is_404(get, url):
 
 
 def test_get_housenumber_with_cia(get, url):
-    housenumber = HouseNumberFactory(number="22", parent__fantoir="1234")
+    housenumber = HouseNumberFactory(number="1", ordinal="A",
+                                     parent__municipality__insee="27638",
+                                     parent__fantoir="276380011")
     resp = get(url('housenumber-resource', id=housenumber.cia,
                    identifier="cia"))
     assert resp.status == falcon.HTTP_200
-    assert resp.json['number'] == "22"
+    assert resp.json['number'] == "1"
 
 
 def test_get_housenumber_with_districts(get, url):
