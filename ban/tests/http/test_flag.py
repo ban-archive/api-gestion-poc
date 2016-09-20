7a1066f7fda5796cb0841bf68763cfafdd46e2d5
Yohan Boniface <yb@enix.org>
2016-09-20 22:04:47 +0200

    test_flag are green

--- ban/tests/http/test_flag.py
+++ ban/tests/http/test_flag.py
@@ -3,33 +3,33 @@ from .utils import authorize
 
 
 @authorize
-def test_can_flag_current_version(client, url):
+def test_can_flag_current_version(client):
     group = GroupFactory()
     version = group.load_version()
-    uri = url('group-version-by-sequential', identifier=group.id, ref=1)
+    uri = '/group/{}/versions/1'.format(group.id)
     resp = client.post(uri, data={'flag': True})
     assert resp.status_code == 200
     assert version.flags.select().count()
 
 
 @authorize
-def test_can_unflag_current_version(client, url, session):
+def test_can_unflag_current_version(client, session):
     group = GroupFactory()
     version = group.load_version()
     version.flag()
     assert version.flags.select().count()
-    uri = url('group-version-by-sequential', identifier=group.id, ref=1)
+    uri = '/group/{}/versions/1'.format(group.id)
     resp = client.post(uri, data={'flag': False})
     assert resp.status_code == 200
     assert not version.flags.select().count()
 
 
 @authorize
-def test_get_version_contain_flags(client, url, session):
+def test_get_version_contain_flags(client, session):
     group = GroupFactory()
     version = group.load_version()
     version.flag()
-    uri = url('group-version-by-sequential', identifier=group.id, ref=1)
+    uri = '/group/{}/versions/1'.format(group.id)
     resp = client.get(uri)
     assert resp.status_code == 200
     assert 'flags' in resp.json
@@ -37,12 +37,12 @@ def test_get_version_contain_flags(client, url, session):
 
 
 @authorize
-def test_can_flag_past_version(client, url):
+def test_can_flag_past_version(client):
     group = GroupFactory()
     group.name = 'Another name'
     group.increment_version()
     group.save()
-    uri = url('group-version-by-sequential', identifier=group.id, ref=1)
+    uri = '/group/{}/versions/1'.format(group.id)
     resp = client.post(uri, data={'flag': True})
     assert resp.status_code == 200
     version = group.load_version(1)
@@ -51,8 +51,8 @@ def test_can_flag_past_version(client, url):
     assert not version.flags.select().count()
 
 
-def test_cannot_flag_without_token(client, url):
+def test_cannot_flag_without_token(client):
     group = GroupFactory()
-    uri = url('group-version-by-sequential', identifier=group.id, ref=1)
+    uri = '/group/{}/versions/1'.format(group.id)
     resp = client.post(uri, data={'flag': True})
     assert resp.status_code == 401
