de032d5b2f709b005c2aec48f25bc2ce008954a6
Yohan Boniface <yb@enix.org>
2016-09-20 17:45:00 +0200

    test_postcode are green

--- ban/tests/http/test_postcode.py
+++ ban/tests/http/test_postcode.py
@@ -5,16 +5,16 @@ from .utils import authorize
 
 
 @authorize
-def test_get_postcode(get, url):
+def test_get_postcode(get):
     postcode = PostCodeFactory(code="09350")
-    resp = get(url('postcode-resource', identifier=postcode.id))
+    resp = get('/postcode/{}'.format(postcode.id))
     assert resp.status_code == 200
     assert resp.json['id']
     assert resp.json['code'] == '09350'
 
 
 @authorize
-def test_create_postcode(client, url):
+def test_create_postcode(client):
     municipality = MunicipalityFactory()
     assert not models.PostCode.select().count()
     data = {
@@ -23,25 +23,25 @@ def test_create_postcode(client, url):
         "municipality": municipality.id,
         "alias": ['Fornex_alias']
     }
-    resp = client.post(url('postcode-post'), data)
+    resp = client.post('/postcode', data)
     assert resp.status_code == 201
     assert resp.json['id']
     assert resp.json['code'] == '09350'
     assert resp.json['name'] == 'Fornex'
     assert resp.json['alias'] == ['Fornex_alias']
     assert models.PostCode.select().count() == 1
-    uri = url('postcode-resource', identifier=resp.json['id'])
+    uri = 'http://localhost/postcode/{}'.format(resp.json['id'])
     assert resp.headers['Location'] == uri
 
 
 @authorize
-def test_postcode_select_use_default_orderby(get, url):
+def test_postcode_select_use_default_orderby(get):
     mun1 = MunicipalityFactory(insee="90002")
     mun2 = MunicipalityFactory(insee="90001")
     PostCodeFactory(code="90102", municipality=mun1)
     PostCodeFactory(code="90102", municipality=mun2)
     PostCodeFactory(code="90101", municipality=mun2)
-    resp = get(url('post_code_collection'))
+    resp = get('/postcode')
     assert resp.status_code == 200
     assert resp.json['total'] == 3
     assert resp.json['collection'][0]['code'] == '90101'
@@ -50,32 +50,32 @@ def test_postcode_select_use_default_orderby(get, url):
 
 
 @authorize
-def test_get_postcode_collection_filtered_by_1_code_param(get, url):
+def test_get_postcode_collection_filtered_by_1_code_param(get):
     PostCodeFactory(code='90000')
     PostCodeFactory(code='91000')
-    resp = get(url('post_code_collection', code='90000'))
+    resp = get('/postcode?code=90000')
     assert resp.status_code == 200
     assert resp.json['total'] == 1
     assert resp.json['collection'][0]['code'] == '90000'
 
 
 @authorize
-def test_get_postcode_collection_filtered_by_2_equals_codes_param(get, url):
+def test_get_postcode_collection_filtered_by_2_equals_codes_param(get):
     PostCodeFactory(code='90000')
     PostCodeFactory(code='91000')
     # 'code' given by the user is used twice but with the same value.
-    resp = get(url('post_code_collection', code=['90000', '90000']))
+    resp = get('/postcode?code=90000&code=90000')
     assert resp.status_code == 200
     assert resp.json['total'] == 1
     assert resp.json['collection'][0]['code'] == '90000'
 
 
 @authorize
-def test_get_postcode_collection_filtered_by_2_diff_codes_param(get, url):
+def test_get_postcode_collection_filtered_by_2_diff_codes_param(get):
     PostCodeFactory(code='90000')
     PostCodeFactory(code='91000')
     # 'code' given by the user is used with 2 differents values.
-    resp = get(url('post_code_collection', code=['90000', '91000']))
+    resp = get('/postcode?code=90000&code=91000')
     assert resp.status_code == 200
     assert resp.json['total'] == 2
     assert resp.json['collection'][0]['code'] == '90000'
@@ -83,12 +83,11 @@ def test_get_postcode_collection_filtered_by_2_diff_codes_param(get, url):
 
 
 @authorize
-def test_get_postcode_collection_can_be_filtered_by_1_code_and_1_pk(get, url):
+def test_get_postcode_collection_can_be_filtered_by_1_code_and_1_pk(get):
     PostCodeFactory(code='90000')
     PostCodeFactory(code='91000')
     # Only 'Code' param will be used to filter, not 'pk' one.
-    params = {'code': '90000', 'pk': '405'}
-    resp = get(url('post_code_collection', **params))
+    resp = get('/postcode?code=90000&pk=405')
     assert resp.status_code == 200
     assert resp.json['total'] == 1
     assert resp.json['collection'][0]['code'] == '90000'
