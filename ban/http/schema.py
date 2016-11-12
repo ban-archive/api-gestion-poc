9d15c75bb1219d3b3567bfb297966990136e463a
flyusfly <fly_fly@popstar.com>
2016-11-12 12:28:48 +0100

    Openapi > Add generic responses in openapi schema

--- ban/http/schema.py
+++ ban/http/schema.py
@@ -45,6 +45,17 @@ BASE = {
             'type': 'string',
             'required': True,
         }
+    },
+    'responses': {
+        '400': {
+            'description': 'Bad Request.'
+        },
+        '401': {
+            'description': 'Unauthorized access.'
+        },
+        '404': {
+            'description': 'Resource does not exist.'
+        },
     }
 }
 
