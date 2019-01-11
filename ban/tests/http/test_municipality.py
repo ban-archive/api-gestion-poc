1dcdded97707364076d43dbb898ddfbca0fccafa
gschittek <gabin.schittek@ign.fr>
2019-01-11 09:30:40 +0100

    Update test_municipality.py

--- ban/tests/http/test_municipality.py
+++ ban/tests/http/test_municipality.py
@@ -269,8 +269,7 @@ def test_patch_municipality_with_alias(patch):
 def test_delete_municipality(client):
     municipality = MunicipalityFactory()
     resp = client.delete('/municipality/{}'.format(municipality.id))
-    assert resp.status_code == 200
-    assert resp.json['resource_id'] == municipality.id
+    assert resp.status_code == 204
     assert not models.Municipality.select().count()
     assert models.Municipality.raw_select().where(
                 models.Municipality.pk == municipality.pk).first().deleted_at
